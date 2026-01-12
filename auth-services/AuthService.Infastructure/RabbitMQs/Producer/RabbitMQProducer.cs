11a42553d4b9570808cd245fe4aeec3ec8edd9fb
PhamTrungDuc <ptrungduc1011@gmail.com>
2026-01-12 17:40:29 +0700

    auth-producer-RabbitMQ

---
+++ auth-services/AuthService.Infastructure/RabbitMQs/Producer/RabbitMQProducer.cs
@@ -0,0 +1,110 @@
+﻿using RabbitMQ.Client;
+using System.Text.Json;
+using System.Text;
+
+namespace auth_services.AuthService.Infastructure.RabbitMQs.Producer
+{
+    public class RabbitMQProducer
+    {
+        private readonly IConfiguration _iConfig;
+
+   
+        public RabbitMQProducer(IConfiguration configuration)
+        {
+            _iConfig = configuration;
+        }
+
+
+        public async Task SendMessage(object message, string routingKey)
+        {
+
+            var factory = new ConnectionFactory
+            {
+
+                HostName = _iConfig["RabbitMQ_Side_Auth:Host"]!,
+                UserName = _iConfig["RabbitMQ_Side_Auth:UserName"]!,
+                Password = _iConfig["RabbitMQ_Side_Auth:Password"]!,
+                ClientProvidedName = "AuthService"
+            };
+
+
+            using var connection = await factory.CreateConnectionAsync();
+            using var channel = await connection.CreateChannelAsync();
+
+
+            //khai báo Exchange
+            await channel.ExchangeDeclareAsync(
+                exchange: _iConfig["RabbitMQ_Side_Auth:Exchange"]!,
+                type: ExchangeType.Direct,
+                durable: true                              // ghi exchange xuống disk  ,  nếu sau này mà RabbitMQ bị tắt thì exchange vẫn còn , không cần tạo lại 
+            );
+
+
+            // khai báo queue
+            await channel.QueueDeclareAsync(
+             queue: _iConfig["RabbitMQ_Side_Auth:Queue:Notification_Email:QueueName"]!,
+             durable: true,                              // ghi queue xuống disk  ,  nếu sau này mà RabbitMQ bị tắt thì  queue vẫn còn , không cần tạo lại 
+             exclusive: false,
+             autoDelete: false
+             );
+
+
+
+
+            await channel.QueueDeclareAsync(
+            queue: _iConfig["RabbitMQ_Side_Auth:Queue:UserInfo:QueueName"]!,
+            durable: true,                              // ghi queue xuống disk  ,  nếu sau này mà RabbitMQ bị tắt thì  queue vẫn còn , không cần tạo lại 
+            exclusive: false,
+            autoDelete: false
+            );
+
+
+            // map(bind) queue với exchange
+            await channel.QueueBindAsync(
+             queue: _iConfig["RabbitMQ_Side_Auth:Queue:Notification_Email:QueueName"]!,
+             exchange: _iConfig["RabbitMQ_Side_Auth:Exchange"]!,
+             routingKey: _iConfig["RabbitMQ_Side_Auth:Queue:Notification_Email:RoutingKey"]!
+            );
+
+
+            await channel.QueueBindAsync(
+            queue: _iConfig["RabbitMQ_Side_Auth:Queue:UserInfo:QueueName"]!,
+            exchange: _iConfig["RabbitMQ_Side_Auth:Exchange"]!,
+            routingKey: _iConfig["RabbitMQ_Side_Auth:Queue:UserInfo:RoutingKey"]!
+           );
+
+            var json = JsonSerializer.Serialize(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+
+            // các thuộc tính của message
+            var properties = new BasicProperties
+            {
+                Persistent = true,     // ghi xuống disk 
+                Headers = new Dictionary<string, object>
+                {
+                    { "producer", "Auth-Service" },
+                    { "service", "Auth-Service" },
+                    { "message_type", "SendEmail" },
+                    { "created_at", DateTime.UtcNow.ToString("O") }
+                }
+            };
+
+
+
+            // push message vào RabbitMQ 
+            await channel.BasicPublishAsync(
+                exchange: _iConfig["RabbitMQ_Side_Auth:Exchange"]!,
+                routingKey: routingKey,
+                mandatory: false,
+                basicProperties: properties,
+                body: body
+            );
+
+        }
+
+    }
+
+
+    // exchange sẽ dựa trên RoutingKey để chuyền message đến đúng queue cần thiết
+}
